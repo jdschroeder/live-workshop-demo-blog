9a53b47bf8705fe738400f8331826d854da1460c
Josh Schroeder <jschroeder@yukonu.ca>
2021-03-03 11:13:40 -0700

    Update index and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About</title>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </main>
+  );
+};
+
+export default AboutPage;
